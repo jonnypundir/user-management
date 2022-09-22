f28223c57af290d6a9a0413ae01c7256e329560f
abhishek <avipandey423@gmail.com>
2022-09-22 16:02:19 +0530

    22/09/2022

--- src/main/java/com/pundir/usermanagement/dto/request/SignupRequest.java
+++ src/main/java/com/pundir/usermanagement/dto/request/SignupRequest.java
@@ -1,5 +1,7 @@
 package com.pundir.usermanagement.dto.request;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,24 +14,30 @@ import java.util.Set;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@ApiModel
 public class SignupRequest {
+    @ApiModelProperty(example = "Abhishek", required = true)
     @NotBlank
     @Size(min = 3, max = 20)
     private String firstName;
 
+    @ApiModelProperty(example = "Pandey", required = true)
     @NotBlank
     @Size(min = 3, max = 20)
     private String lastName;
 
+    @ApiModelProperty(example = "abc@gmail.com", required = true)
     @NotBlank
     @Size(max = 50)
     @Email
     private String email;
 
+    @ApiModelProperty(example = "8487..", required = true)
     @NotBlank
     @Size(max = 10)
     private Long contact;
 
+    @ApiModelProperty(example = "Abc#123..", required = true)
     @NotBlank
     @Size(min = 6, max = 40)
     private String password;

ba386147e8eae1b90e0004450d291bf6962c16bb
abhishek <avipandey423@gmail.com>
2022-09-22 12:28:00 +0530

    22/09/2022

--- src/main/java/com/pundir/usermanagement/services/SignupServiceImpl.java
+++ src/main/java/com/pundir/usermanagement/services/SignupServiceImpl.java
@@ -1,11 +1,17 @@
 package com.pundir.usermanagement.services;
 
 import com.pundir.usermanagement.dto.request.SignupRequest;
+import com.pundir.usermanagement.entities.ERole;
+import com.pundir.usermanagement.entities.Role;
 import com.pundir.usermanagement.entities.User;
+import com.pundir.usermanagement.repository.RoleRepository;
 import com.pundir.usermanagement.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Service
 public class SignupServiceImpl implements SignupService {
@@ -13,16 +19,22 @@ public class SignupServiceImpl implements SignupService {
     PasswordEncoder encoder;
     @Autowired
     UserRepository userRepository;
-
+    @Autowired
+    RoleRepository roleRepository;
     @Override
     public User doSignup(SignupRequest signupRequest) {
+
+        Role userRole = roleRepository.findByName(ERole.ROLE_USER).orElseThrow(() ->
+                new RuntimeException("Error: Role is not found."));                                            ;
+        Set<Role> rolesSet = new HashSet<>();
+        rolesSet.add(userRole);
         // Create new user's account
         User user = User.builder()
                 .email(signupRequest.getEmail())
                 .contact(signupRequest.getContact())
                 .username(signupRequest.getUsername())
                 .password(encoder.encode(signupRequest.getPassword()))
-                //.roles(Set.of(new Role("",ERole.ROLE_USER)))
+                .roles(rolesSet)
                 .build();
         System.out.println(user);
         userRepository.save(user);
